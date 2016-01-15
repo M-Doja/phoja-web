06c2bfea2afe05cae0f1e518fc3540a1626472fd
M-Doja <micdoja@yahoo.com>
2016-01-14 18:26:16 -0800

    add drop down menu

--- public/javascript/controller/AddBlogController.js
+++ public/javascript/controller/AddBlogController.js
@@ -1,5 +1,25 @@
-angular.module('BlogApp').controller('AddBlogController', function($http, $state){
+angular.module('Phoja').controller('AddBlogController', function($http, $state){
   var vm = this;
+  // Create the dropdown base
+  $("<select />").appendTo("#nav");
 
+  // Create default option "Go to..."
+  $("<option />", {
+     "selected": "selected",
+     "value"   : "",
+     "text"    : "Go to..."
+  }).appendTo("#nav select");
 
+  // Populate dropdown with menu items
+  $("#nav a").each(function() {
+   var el = $(this);
+   $("<option />", {
+       "value"   : el.attr("href"),
+       "text"    : el.text()
+   }).appendTo("#nav select");
+  });
+
+});
+$("#nav select").change(function() {
+  window.location = $(this).find("option:selected").val();
 });
